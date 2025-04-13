06371c7c64371accadd0c195288701fc0d19cc4d
Zhalslar <zsl2936169201@gmail.com>
2025-04-13 12:52:07 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,47 @@
+
+<div align="center">
+
+# asrebot_plugin_password
+
+[![License](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+[![Python 3.8+](https://img.shields.io/badge/Python-3.8%2B-blue.svg)](https://www.python.org/)
+[![AstrBot](https://img.shields.io/badge/AstrBot-3.4%2B-orange.svg)](https://github.com/Soulter/AstrBot)
+
+</div>
+
+## 📦 介绍
+
+- 本插件提供方法强制修改astrbot面板的用户名、密码（仅管理员可用），无需输入原始密码，用户再也不用担心忘记密码。
+
+## 📦 安装
+
+- 从插件市场搜索asrebot_plugin_password，点击安装即可。
+- 安装不成功也可以尝试直接用命令行下载：
+
+```bash
+# 克隆仓库到插件目录
+cd /AstrBot/data/plugins
+git clone https://github.com/Zhalslar/asrebot_plugin_password
+
+# 控制台重启AstrBot
+```
+
+## ⌨️ 使用命令
+
+### 基础命令
+
+```plaintext
+/修改密码 xxx
+```
+
+## 📌 注意事项
+
+- 仅管理员可用命令修改密码，不必担心被人串改密码
+
+## 🌟 支持
+
+- Star 这个项目！
+
+## 📜 开源协议
+
+本项目采用 [MIT License](LICENSE)
---
+++ main.py
@@ -0,0 +1,101 @@
+import re
+import json
+import hashlib
+from astrbot.api.star import Context, Star, register
+from astrbot import logger
+from astrbot.api.event import filter
+from astrbot.core.platform.astr_message_event import AstrMessageEvent
+from astrbot.core.star.filter.permission import PermissionType
+
+CMD_CONFIG_PATH = "data/cmd_config.json"
+
+
+@register(
+    "asrebot_plugin_password",
+    "Zhalslar",
+    "强制修改astrbot的面板登录密码（仅管理员可用）",
+    "1.0.0",
+    "https://github.com/Zhalslar/astrbot_plugin_password",
+)
+class Box(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    def _load_json_data(self) -> dict:
+        """加载并解析 JSON 文件，去除 BOM"""
+        with open(CMD_CONFIG_PATH, "r", encoding="utf-8") as file:
+            content = file.read()
+            if content.startswith("\ufeff"):
+                content = content[1:]  # 去除 BOM
+            data = json.loads(content)
+        return data
+
+    def _save_json_data(self, data: dict):
+        """将数据保存到 JSON 文件"""
+        with open(CMD_CONFIG_PATH, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    def _is_valid(self, password: str) -> bool:
+        """验证字符是否符合要求"""
+        # 密码长度至少为4位
+        if len(password) < 4:
+            return False
+        # 密码只能包含大小写字母、数字和特殊字符
+        if not re.match(r"^[A-Za-z0-9!@#$%^&*(),.?\":{}|<>]+$", password):
+            return False
+        return True
+
+    @filter.permission_type(PermissionType.ADMIN)
+    @filter.command("修改用户名")
+    async def change_username(
+        self, event: AstrMessageEvent, input_username: int | str | None = None
+    ):
+        """/修改用户名 xxx"""
+        if not input_username:
+            yield event.plain_result("未输入新用户名")
+            return
+        # 兼容int和str
+        new_username = str(input_username)
+
+        # 更新 JSON 数据中的用户名 (同时确保键存在)
+        data = self._load_json_data()
+        if "dashboard" not in data:
+            data["dashboard"] = {}
+        data["dashboard"]["username"] = new_username
+        self._save_json_data(data)
+
+        yield event.plain_result(f"用户名已更新为{new_username}，\n重启bot后生效")
+        logger.info(f"Astrbot的面板用户名已更新为{new_username}")
+
+    @filter.permission_type(PermissionType.ADMIN)
+    @filter.command("修改密码")
+    async def change_password(
+        self, event: AstrMessageEvent, input_password: int | str | None = None
+    ):
+        """/修改密码 xxx"""
+        if not input_password:
+            yield event.plain_result("未输入新密码")
+            return
+        # 兼容int和str
+        new_password = str(input_password)
+
+        # 验证新密码是否符合要求
+        if not self._is_valid(new_password):
+            yield event.plain_result(
+                "密码不符合要求，请确保密码仅包含大小写字母、数字和特殊字符，且长度至少为4位"
+            )
+            return
+
+        # 将新密码转换为 MD5 哈希值
+        md5_hash = hashlib.md5(new_password.encode("utf-8")).hexdigest()
+
+        # 更新 JSON 数据中的密码 (同时确保键存在)
+        data = self._load_json_data()
+        if "dashboard" not in data:
+            data["dashboard"] = {}
+        data["dashboard"]["password"] = md5_hash
+        self._save_json_data(data)
+
+        masked_password = str(new_password)[0] + "*" * (len(str(new_password)) - 1)
+        yield event.plain_result(f"密码已更新为{masked_password}，\n重启bot后生效")
+        logger.info(f"Astrbot的面板登录密码已更新为{new_password}")
